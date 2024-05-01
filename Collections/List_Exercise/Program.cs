cf63e59f54cb6ba074f10a360b9f851a9e3a7586
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-05-01 15:24:11 +0300

    MergeLists Done

--- Collections/List_Exercise/Program.cs
+++ Collections/List_Exercise/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        string action = "";
+
+        List<string> list = new List<string>();
+
+        ListActions listActions = new();
+
+        while (action != "end")
+        {
+            try
+            {
+                if (list.Count == 0)
+                {
+                    Console.Write("Enter your List: ");
+                    list = Console.ReadLine().Trim().Split(" ").ToList();
+                    if (string.IsNullOrEmpty(list[0]))
+                    {
+                        list.Clear();
+                        throw new Exception("List cannot be empty!");
+                    }
+                }
+
+                Console.Write("\nEnter your action: ");
+                action = Console.ReadLine().ToLower();
+
+                if (string.IsNullOrEmpty(action))
+                {
+                    throw new Exception("Invalid Action!");
+                }
+
+                switch (action)
+                {
+                    case "mergelists":
+                        listActions.MergeLists(list);
+                        break;
+
+                    default:
+                        throw new Exception("Invalid Action!");
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+    }
+}
+
+class ListActions
+{
+    public void MergeLists(List<string> main_list)
+    {
+        Console.Write("How many list to add: ");
+        int number_of_lists = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Where to add the list/s: ");
+        int placeAt = int.Parse(Console.ReadLine());
+
+        List<string> merging_list = new();
+
+        for (int i = 1; i <= number_of_lists; i++)
+        {
+            Console.Write($"List {i}: ");
+            merging_list.InsertRange(placeAt,Console.ReadLine().Split(" ").ToList());
+        }
+
+        main_list.AddRange(merging_list);
     }
 }
