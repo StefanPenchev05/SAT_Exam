c9b9ce349d1ff77f95d5fb4eaedca37cffa0ec45
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-05-01 13:36:56 +0300

    Added adding elements in List

--- Collections/Lists/Program.cs
+++ Collections/Lists/Program.cs
@@ -2,16 +2,18 @@
 
 class Program
 {
+    // Create new list of integers, List<T>, while T is generic
+
+    private List<int> ints = new List<int>();
     static void Main(string[] args)
     {
-        BasicUse();
+        Program program = new Program();
+        program.BasicUse();
+        program.AddElement();
     }
 
-    private static void BasicUse()
+    private void BasicUse()
     {
-        // Create new list of integers, List<T>, while T is generic
-        List<int> ints = new List<int>();
-
         // Add elements in the list
         ints.Add(1);
         ints.Add(3);
@@ -36,12 +38,51 @@ class Program
         // Check if the list contains a certain value
         Console.WriteLine($"{ints.Contains(10)}"); // true
 
-        // Iterate over the list
-        Console.WriteLine("Iterating over the list: ");
-        foreach(var el in ints)
+        PrintList();
+
+    }
+
+    private void AddElement()
+    {
+        // Add method: Adds an element at the end of the list
+        ints.Add(4);
+
+        // Insert Method: Inserts an element at specific position(index)
+        ints.Insert(0, 10);
+
+        // AddRange method: Adds the elements of the specified collection at the end of the list
+        ints.AddRange(new int[] { 3, 7, 2, 8 });
+
+        // InsertRange method: Inserts the element of specified collection at specific position(index)
+        ints.InsertRange(3, new List<int> {2345,5654});
+
+        PrintList();
+    }
+
+    private void PrintList()
+    {
+        // The most command use for printing or iterating over the list is with for and foreach loops or string.Join
+
+        // For loop
+        Console.Write("Printing list with For loop: ");
+        for (int i = 0; i < ints.Count - 1; i++)
         {
-            Console.WriteLine(el);
+            Console.Write($"{ints[i]} ");
         }
 
+        Console.WriteLine();
+
+        // Foreach loop
+        Console.Write("Printing list with ForEach loop: ");
+        foreach (var el in ints)
+        {
+            Console.Write($"{el} ");
+
+        }
+
+        Console.WriteLine();
+
+
+        Console.WriteLine($"Printig list with string.Join: {string.Join(" ", ints)}");
     }
 }
