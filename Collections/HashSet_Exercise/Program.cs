df8a68fc085183808a4d631be286ee15573a0103
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-05-02 16:43:01 +0300

    Add remove contact functionality

--- Collections/HashSet_Exercise/Program.cs
+++ Collections/HashSet_Exercise/Program.cs
@@ -26,6 +26,10 @@ class Program
                         bool isAdded = AddContact();
                         if (isAdded) Console.WriteLine("Contact Added Successfully");
                         break;
+                    case "remove":
+                        bool isRemoved = RemoveContact();
+                        if (isRemoved) Console.WriteLine("Contact Removed Successfully");
+                        break;
                     case "end":
                         break;
                     default:
@@ -48,13 +52,13 @@ class Program
         {
             try
             {
-                Console.WriteLine("Enter exit, if you do not want to add new contact");
+                Console.WriteLine("Enter exit, if you do not want to add a new contact");
                 Console.Write("Please enter the number of the contact: ");
                 number = Console.ReadLine() ?? "";
 
                 if (number.Length == 0) throw new Exception("You have to pass the number of the new contact!");
                 else if (number == "exit") break;
-    
+
 
                 bool isAdded = contacts.Add(number);
                 if (!isAdded) throw new Exception("This contact already exists");
@@ -68,6 +72,35 @@ class Program
             }
         }
 
+        return false;
+    }
+
+    private static bool RemoveContact()
+    {
+        string contact;
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Enter exit, if you do not want to remove a contact");
+                Console.Write("Please enter the number of the contact: ");
+                contact = Console.ReadLine() ?? "";
+
+                if (contact.Length == 0) throw new Exception("You have to pass the number of the new contact!");
+                else if (contact == "exit") break;
+
+                bool isRemoved = contacts.Remove(contact);
+                if (!isRemoved) throw new Exception("This contact does not exists");
+
+                return true;
+
+            }
+            catch (Exception err)
+            {
+                Console.Clear();
+                Console.WriteLine(err.Message);
+            }
+        }
         return false;
     }
 }
