88c510ecea4f54f3f9907010d3eb7b5b15c240d8
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-05-03 16:53:58 +0300

    Add Queue and Stack projects to the solution

---
+++ Collections/Queue/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Queue;
+
+
+/// <summary>
+/// Queue<T>
+/// A generic collection that represents a first-in, first-out collection object
+/// Elements added to the end of the Queue and removed fmro the start.
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.BasicUse();
+    }
+
+    private void BasicUse()
+    {
+        // Create a new Queue
+        Queue<int> numbers = new();
+
+        // Enqueue method: Adds an object to the end of the of the Queue<T>
+        numbers.Enqueue(1);
+        numbers.Enqueue(2);
+        numbers.Enqueue(3);
+        numbers.Enqueue(4);
+
+        // The queue now contains {1, 2, 3, 4}
+
+        // Dequeue method: Removes and returns the object at the beginning of the Queue<T>
+        int first = numbers.Dequeue(); // first = 1
+
+        // the queue now contains {2, 3, 4}
+
+        // Peek method: Returns the obkect at the beginning of the Queue<T>
+        int peek = numbers.Peek(); // peek = 2
+
+        // Count: Gets the number of elements contained in the Queue<T>.
+        int count = numbers.Count; // count = 3
+
+        // Clear: Removes all objects from the Queue<T>.
+        //numbers.Clear();
+
+        // The queue is now empty
+
+        // Check if queue contains a number
+        bool contains = numbers.Contains(2); // contains = true
+
+        // Trim excess
+        numbers.TrimExcess();
+
+        // Enumerate the queue
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ Collections/Stack/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Stack;
+
+/// <summary>
+/// Stack<T>
+/// A generic collection that represents a last-in, first-out collection of objects.
+/// Elements are added to the top of the Stack and removed from the top.
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.BasicUse();
+    }
+
+    private void BasicUse()
+    {
+        // Create a new Stack
+        Stack<int> numbers = new Stack<int>();
+
+        // Push: Adds an object to the top of the Stack<T>.
+        numbers.Push(1);
+        numbers.Push(2);
+        numbers.Push(3);
+        numbers.Push(4);
+
+        // The stack now contains: 4, 3, 2, 1
+
+        // Pop: Removes and returns the object at the top of the Stack<T>.
+        int top = numbers.Pop(); // top = 4
+
+        // The stack now contains: 3, 2, 1
+
+        // Peek: Returns the object at the top of the Stack<T> without removing it.
+        int peek = numbers.Peek(); // peek = 3
+
+        // The stack still contains: 3, 2, 1
+
+        // Count: Gets the number of elements contained in the Stack<T>.
+        int count = numbers.Count; // count = 3
+
+        // Clear: Removes all objects from the Stack<T>.
+        //numbers.Clear();
+
+        // The stack is now empty
+
+        // Check if stack contains a number
+        bool contains = numbers.Contains(2); // contains = true
+
+        // Enumerate the stack
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
