f88aa9a61e0d48d9d678ae2df73919cc7b2d9eac
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-05-14 12:00:51 +0300

    chore: Add Zad25_2022 and Zad26 projects to the solution

---
+++ 2022/Zad25_2022/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Zad25_2022;
+
+class Program
+{
+    static Dictionary<int, int> number_of_digits = new Dictionary<int, int>();
+    static void Main(string[] args)
+    {
+        int len_of_nums = 0;
+        while (len_of_nums == 0)
+        {
+            if (int.TryParse(Console.ReadLine(), out len_of_nums))
+            {
+                Console.Clear();
+                break;
+            }
+            Console.WriteLine("Invalid input. Enter only Numbers");
+        }
+
+        Console.WriteLine(len_of_nums);
+        List<int> userInput = new List<int>();
+        GetUserInputAndValidate(ref userInput, len_of_nums);
+
+        foreach(var num in userInput)
+        {
+            if(number_of_digits.ContainsKey(num))
+            {
+                number_of_digits[num] += 1;
+                continue;
+            } 
+            else
+            {
+                number_of_digits.Add(num, 1);
+            }
+        }
+
+        PrintCoutOfDigits();
+
+    }
+
+    static void GetUserInputAndValidate(ref List<int> user_input_list, int len_of_user_input)
+    {
+        for (int i = 0; i < len_of_user_input; i++)
+        {
+            while (true)
+            {
+                int number = 0;
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    user_input_list.Add(number);
+                    break;
+                }
+
+                Console.WriteLine("Invalid input. Enter only Numbers");
+            }
+        }
+    }
+
+    static void PrintCoutOfDigits()
+    {
+        foreach(KeyValuePair<int,int> kv in number_of_digits)
+        {
+            Console.WriteLine("число: {0} , брой: {1}", kv.Key, kv.Value);
+        }
+    }
+}
---
+++ 2022/Zad26/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Drawing;
+
+namespace Zad26;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Item : IComparable<Item>
+{
+    private string _description;
+    public string Description
+    {
+        get { return _description; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Description cannot be null or empty.");
+            }
+
+            _description = value;
+        }
+    }
+
+    private int _price;
+    public int Price
+    {
+        get { return _price; }
+        set
+        {
+            if (!(value >= 0))
+            {
+                throw new ArgumentException("Price cannot be negative number");
+            }
+
+            _price = value;
+        }
+    }
+
+    public Item(string description, int price)
+    {
+        Description = description;
+        Price = price;
+    }
+
+    public int CompareTo(Item? other)
+    {
+        if (other == null)
+            return 1;
+
+        int descriptionComparison = this.Description.CompareTo(other.Description);
+        if(descriptionComparison == 0)
+            return this.Price.CompareTo(other.Price);
+        else
+            return descriptionComparison;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Description} ({this.Price})";
+    }
+}
+
+class ItemList
+{
+    private List<Item> Items = new List<Item>();
+
+    public int Size()
+    {
+        return Items.Count();
+    }
+
+    public Item Get(int index)
+    {
+        if(index > Items.Count || index < Items.Count)
+            throw new ArgumentException("Index is not valid");
+
+        return Items[index];
+    }
+
+
+}
