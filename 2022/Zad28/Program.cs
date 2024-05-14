e0da50036338efea92a3ed15d41ba0cd1b6d0f94
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-05-14 19:03:41 +0300

    chore: Add Zad28 project to the solution and implement RemoveElementsDivisibleByK method

---
+++ 2022/Zad28/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Zad28;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+
+    static void RemoveElementsDivisibleByK(ref List<int> nums, int k)
+    {
+        for (int i = nums.Count - 1; i >= 0; i--)
+        {
+            int currentNum = nums[i];
+            int sumOfNums = 0;
+            while (currentNum > 0)
+            {
+                sumOfNums += currentNum % 10;
+                currentNum /= 10;
+            }
+
+            if (sumOfNums % k == 0)
+            {
+                nums.RemoveAt(i);
+            }
+        }
+    }
+}
